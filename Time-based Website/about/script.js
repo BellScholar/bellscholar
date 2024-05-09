6da982fcccec78465898f856bc67043b2a873a29
BellScholar <146101580+BellScholar@users.noreply.github.com>
2024-05-09 03:43:50 -0400

    Create script.js

---
+++ Time-based Website/about/script.js
@@ -0,0 +1,3 @@
+document.getElementById("navigateButton").addEventListener("click", function() {
+    window.location.href = 'https://bellscholar.github.io/bellscholar/Time-based Website/index.html';
+  });
